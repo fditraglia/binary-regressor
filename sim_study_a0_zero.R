ce84553d36e81232f5034cb1ab2b89a98ca13818
Francis DiTraglia <fditra@sas.upenn.edu>
2016-12-11 17:41:28 -0500

    implement inference for reduced form: the expressions from our notes are indeed correct

--- sim_study_a0_zero.R
+++ sim_study_a0_zero.R
@@ -14,6 +14,60 @@ dgp <- function(a1, b = 1, n = 1000, d = 0.15, rho = 0.5){
   return(data.frame(Tstar, Tobs, y, z))
 }
 
+get_m_star <- function(d = 0.15, r = 0.5, c = 0){
+  g0 <- qnorm(d)
+  g1 <- qnorm(1 - d) - qnorm(d)
+  
+  f_U <- function(x, a) x * dnorm(x) * pnorm((a - r * x) / sqrt(1 - r^2))
+  mstar00 <- c + integrate(function(x) f_U(x, -g0), -Inf, Inf)$value / 
+    pnorm(-g0)
+  mstar01 <- c + integrate(function(x) f_U(x, -(g0 + g1)), -Inf, Inf)$value / 
+    pnorm(-(g0 + g1))
+  
+  f_L <- function(x, a) x * dnorm(x) * (1 - pnorm((a - r * x) / sqrt(1 - r^2)))
+  mstar10 <- c + integrate(function(x) f_L(x, -g0), -Inf, Inf)$value / 
+    (1 - pnorm(-g0))
+  mstar11 <- c + integrate(function(x) f_L(x, -(g0 + g1)), -Inf, Inf)$value / 
+    (1 - pnorm(-(g0 + g1)))
+  out <- c(mstar00, mstar01, mstar10, mstar11) 
+  names(out) <- c('mstar00', 'mstar01', 'mstar10', 'mstar11')
+  return(out)
+}
+
+# This is just a Monte Carlo version used to check the preceding function 
+get_m_star_sim <- function(d = 0.15, r = 0.5, c = 0, n_sims = 1000000){
+  g0 <- qnorm(d)
+  g1 <- qnorm(1 - d) - qnorm(d)
+  S <- matrix(c(1, r, r, 1), 2, 2, byrow = TRUE)
+  draws <- mvtnorm::rmvnorm(n_sims, sigma = S)
+  epsilon <- draws[, 1]
+  xi <- draws[, 2]
+  mstar00 <- c + mean(epsilon[xi <= -g0])
+  mstar01 <- c + mean(epsilon[xi <= -(g0 + g1)])
+  mstar10 <- c + mean(epsilon[xi > -g0])
+  mstar11 <- c + mean(epsilon[xi > -(g0 + g1)])
+  out <- c(mstar00, mstar01, mstar10, mstar11) 
+  names(out) <- c('mstar00', 'mstar01', 'mstar10', 'mstar11')
+  return(out)
+}
+
+# Check the theoretical calculation for the asymptotic variance of the reduced
+# form estimator for the special case of our simulation DGP, in which the
+# non-compliance is symmetric and q is fixed at 0.5
+get_AVAR_rf <- function(b, d = 0.15, r = 0.5, c = 0){
+  mstar <- get_m_star(d, r, c)
+  m_diff <- mstar[4] - mstar[2]
+  out <- 4 * (1 + d * (1 - d) * b * (b + 2 * m_diff))
+  return(out)
+}
+get_AVAR_rf_sim <- function(b, d = 0.15, r = 0.5){
+  sims <- dgp(a1 = 0.1, b, 1000000, d, r)
+  y_z0 <- sims$y[sims$z == 0]
+  y_z1 <- sims$y[sims$z == 1]
+  eta <- c(y_z0 - mean(y_z0), y_z1 - mean(y_z1))
+  return(4 * var(eta))
+}
+  
 
 get_V_true <- function(a1, b, d = 0.15, n_sims = 1000000){
 
@@ -71,18 +125,21 @@ get_V_true <- function(a1, b, d = 0.15, n_sims = 1000000){
 
 
 est <- function(dat){
-  #reg_RF <- lm(y ~ z, dat)
-  #summary_RF <- summary(RF)
-  #RF <- summary_RF$coefficients[2, 1]
-  #RF_SE <- summary_RF$coefficients[2, 2]
-
   n <- nrow(dat)
   q <- with(dat, mean(z))
   p <- with(dat, mean(Tobs))
   mu <- with(dat, mean(y))
   s <- with(dat, mean(y^2))
   r <- with(dat, mean(y * Tobs))
-
+ 
+  # Inference for Reduced Form slope coefficient
+  RF <- with(dat, mean(y[z == 1]) - mean(y[z == 0]))
+  eta <- with(dat, y - z * mean(y[z == 1]) - (1 - z) * mean(y[z == 0])) 
+  s2_eta <- var(eta)
+  E_z_eta2 <- mean(dat$z * eta^2)
+  RF_AVAR <- s2_eta / (1 - q)^2 + ((1 - 2 * q) / (q^2 * (1 - q^2))) * E_z_eta2
+  RF_SE <- sqrt(RF_AVAR / n)
+  
   s_T_z <- with(dat, cov(Tobs, z))
   s_y_z <- with(dat, cov(y, z))
   s_y2_z <- with(dat, cov(y^2, z))
@@ -126,16 +183,17 @@ est <- function(dat){
   V_theta <- V[1:2, 1:2]
   b_SE <- sqrt(V_theta[1, 1] / n)
   a1_SE <- sqrt(V_theta[2, 2] / n)
-  return(c('b' = b, 'b_SE' = b_SE, 'a1' = a1, 'a1_SE' = a1_SE))
+  return(c('b' = b, 'b_SE' = b_SE, 'a1' = a1, 'a1_SE' = a1_SE, 
+           'RF' = RF, 'RF_SE' = RF_SE))
 }
 
 plot_dist <- function(a1, b, n, d = 0.15, rho = 0.5){
   V_true <- get_V_true(a1, b, d)
   b_SE_true <- sqrt(V_true[1, 1] / n)
   a1_SE_true <- sqrt(V_true[2, 2] / n)
-  RF_SE_true <- sqrt(4 / n)
+  RF_SE_true <- sqrt(get_AVAR_rf(b, d, rho) / n)
   sim_draws <- as.data.frame(t(replicate(10000, est(dgp(a1, b, n, d)))))
-  par(mfrow = c(1, 2))
+  par(mfrow = c(1, 3))
   par(oma = c(0, 0, 2, 0))
 
   MASS::truehist(sim_draws$b, col = 'lightskyblue', xlab = expression(beta))
@@ -155,12 +213,25 @@ plot_dist <- function(a1, b, n, d = 0.15, rho = 0.5){
   a1_true_density <- dnorm(a1_seq, mean = a1, sd = a1_SE_true)
   points(a1_seq, a1_true_density, type = 'l', lwd = 3, lty = 1)
   abline(v = a1, lty = 1, lwd = 3, col = 'firebrick')
+  
+  MASS::truehist(sim_draws$RF, col = 'lightskyblue', 
+                 xlab = expression(beta * (1 - 2 * delta)))
+  RF_bias_text <- paste0('Bias = ', round(mean(sim_draws$RF) - b * (1 - 2 * d), 3))
+  RF_SD_text <- paste0('SD = ', round(sd(sim_draws$RF), 3))
+  title(paste(RF_bias_text, ',', RF_SD_text), font.main = 1, cex.main = 1)
+  RF_seq <- seq(from = min(sim_draws$RF), to = max(sim_draws$RF), length.out = 1000)
+  RF_true_density <- dnorm(RF_seq, mean = b * (1 - 2 * d), sd = RF_SE_true)
+  points(RF_seq, RF_true_density, type = 'l', lwd = 3, lty = 1)
+  abline(v = b * (1 - 2 * d), lty = 1, lwd = 3, col = 'firebrick')
+  
   mytitle <- substitute(paste(beta, ' = ', my_beta, ', ', alpha[1],
                                ' = ', my_alpha1, ', ',
                               delta, ' = ', my_delta,
                               ', ', n, ' = ', my_n),
                         list(my_beta = b, my_alpha1 = a1, my_n = n,
                              my_delta = d))
+  
+  
   title(main = mytitle, outer = T)
   par(mfrow = c(1, 1))
   par(oma = c(0, 0, 0, 0))
