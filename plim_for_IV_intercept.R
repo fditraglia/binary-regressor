31011b56fc6e8abc2f171e65aeb78731e27f5b2b
Francis DiTraglia <fditra@sas.upenn.edu>
2017-02-27 10:49:32 -0500

    finish updating talk for today at the lunch seminar!

--- plim_for_IV_intercept.R
+++ plim_for_IV_intercept.R
@@ -2,7 +2,7 @@ dgp_intercept <- function(a0, a1, c = 0, b = 1, n = 1000, d = 0.15, rho = 0.5){
   n_treat <- ceiling(n/2)
   n_control <- n - n_treat
   z <- c(rep(0, n_control), rep(1, n_treat)) # offer of treatment
-  errors <- rmvnorm(n, sigma = matrix(c(1, rho, rho, 1), 2, 2, byrow = TRUE))
+  errors <- mvtnorm::rmvnorm(n, sigma = matrix(c(1, rho, rho, 1), 2, 2, byrow = TRUE))
   g0 <- qnorm(d)
   g1 <- qnorm(1 - d) - qnorm(d)
   Tstar <- as.numeric(g0 + g1 * z + errors[,2] > 0) #select into treatment
@@ -13,10 +13,10 @@ dgp_intercept <- function(a0, a1, c = 0, b = 1, n = 1000, d = 0.15, rho = 0.5){
 }
 
 set.seed(637)
-a0_true <- 0.25
-a1_true <- 0.07
-c_true <- -1
-b_true <- 1.3
+a0_true <- 0.28753
+a1_true <- 0.089924
+c_true <- 3
+b_true <- 0.2
 foo <- dgp_intercept(a0_true, a1_true, c_true, b_true, 1e7)
 Z_tilde <- cbind(rep(1, nrow(foo)), foo$z)
 T_tilde <- cbind(rep(1, nrow(foo)), foo$Tobs)
