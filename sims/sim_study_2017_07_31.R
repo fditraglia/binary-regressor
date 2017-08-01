28cf53e6e4c440e8e3fe8471f65cd537badbf119
Francis DiTraglia <fditra@sas.upenn.edu>
2017-08-01 15:49:59 -0400

    first step of sim study: check size for joint test of (a0, a1) equal to the truth

--- sims/sim_study_2017_07_31.R
+++ sims/sim_study_2017_07_31.R
@@ -1,12 +1,9 @@
 library(mbereg)
 library(parallel)
 
-#b_seq <- c(0, 0.2, 0.5, 1, 2)
-b_seq <- c(0, 0.2)
-#n_seq <- c(1000, 5000)
-n_seq <- c(1000)
-#a0_seq <- a1_seq <- c(0, 0.1, 0.2, 0.3)
-a0_seq <- a1_seq <- c(0, 0.1)
+b_seq <- c(0, 0.1, 0.2, 0.5, 1, 2)
+n_seq <- c(250, 500, 1000, 2000)
+a0_seq <- a1_seq <- c(0, 0.1, 0.2, 0.3, 0.4)
 d_seq <- c(0.15)
 rho_seq <- c(0.5)
 cc_seq <- c(0)
@@ -23,19 +20,19 @@ nB <- 5000
 normal_sims <- matrix(rnorm(nB * 6), nrow = 6, ncol = nB)
 
 get_size_alpha_i <- function(i) {
-  sim_alphas_size(true_params = sim_params[i,], normal_sims)
+  sim_alphas_size(true_params = sim_params[i,], normal_sims, nreps = 10000)
 }
-size_alpha <- mclapply(1:nrow(sim_params), get_size_alpha_i, mc.cores = 8)
+system.time(size_alpha <- mclapply(1:nrow(sim_params), get_size_alpha_i, mc.cores = 8))
 
-get_bonf_CI_i <- function(i) {
-  sim_bonf_CI(true_params = sim_params[i,], normal_sims,
-              delta1 = 0.05, delta2 = 0.05, nreps = 10)
-}
-CIs_bonf <- mclapply(1:nrow(sim_params), get_bonf_CI_i, mc.cores = 8)
-
-results <- list(size_alpha = size_alpha, CIs_bonf = CIs_bonf)
+# get_bonf_CI_i <- function(i) {
+#   sim_bonf_CI(true_params = sim_params[i,], normal_sims,
+#               delta1 = 0.05, delta2 = 0.05)
+# }
+# CIs_bonf <- mclapply(1:nrow(sim_params), get_bonf_CI_i, mc.cores = 8)
+#
+# results <- list(size_alpha = size_alpha, CIs_bonf = CIs_bonf)
 
 setwd('~/binary-regressor/sims/')
-save(results, file = 'results_2017_07_31.Rd')
+save(size_alpha, file = 'size_alpha_2017_07_31.Rd')
 
 
