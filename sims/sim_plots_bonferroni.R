af0d6e20e2086c1e9fcfeeeaa850e5c61911e936
Francis DiTraglia <fditra@sas.upenn.edu>
2017-08-10 13:04:44 -0400

    so close to finishing this proof!

--- sims/sim_plots_bonferroni.R
+++ sims/sim_plots_bonferroni.R
@@ -11,7 +11,10 @@ CI_plot <- function(x, CIs){
   plot(x, coverage, type = 'l', xlab = '')
 }
 
-i <- 12
+#i <- 12
+#i <- 16
+#i <- 13
+i <- 14
 true_params <- results$params[i,]
 
 b_true <- true_params$b
@@ -20,6 +23,8 @@ a1_true <- true_params$a1
 s_true <- 1 - a0_true - a1_true
 n_true <- true_params$n
 theta1_true <- b_true / s_true
+d_true <- true_params$d
+RF_true <- b_true * (1 - 2 * d_true)
 
 CIs <- results$CIs_bonf[[i]]
 
@@ -28,15 +33,20 @@ s_CIs <- do.call(rbind, lapply(CIs, function(x) x$s))
 b_CIs <- do.call(rbind, lapply(CIs, function(x) x$b))
 
 
-b_seq <- seq(0, 2 * b_true, 0.01)
+b_seq <- seq(0, 2, 0.01)
 CI_plot(b_seq, b_CIs)
+abline(v = b_true, col = 'red', lty = 2)
+abline(v = theta1_true, col = 'blue', lty = 2)
+abline(v = RF_true, col = 'blue', lty = 2)
+abline(h = 0.90, col = 'black', lty = 2)
 
 s_seq <- seq(0, 0.99, 0.01)
 CI_plot(s_seq, s_CIs)
 abline(v = s_true, col = 'red', lty = 2)
 abline(h = 0.95, col = 'blue', lty = 2)
 
-theta1_seq <- seq(0, 2 * theta1_true, 0.01)
+theta1_seq <- seq(-1, 1, 0.01)
 CI_plot(theta1_seq, theta1_CIs)
 abline(v = theta1_true, col = 'red', lty = 2)
 abline(h = 0.95, col = 'blue', lty = 2)
+
