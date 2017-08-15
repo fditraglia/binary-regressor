9f817371c18cc91cb5c07e3649003e39c34be7f4
Francis DiTraglia <fditra@sas.upenn.edu>
2017-08-15 14:46:23 -0400

    add bonferroni plots to paper

--- sims/sim_plots_bonferroni.R
+++ sims/sim_plots_bonferroni.R
@@ -1,91 +1,73 @@
+library(mbereg)
+library(tikzDevice)
 setwd('~/binary-regressor/sims/')
 
 load('bonf-CIs-2017-08-07.Rd')
 results_weak <- results
 rm(results)
 
-load('bonf-CIs-2017-08-06.Rd')
-results_weak_d1small <- results
-rm(results)
-
-load('bonf-CIs-nondiff-2017-08-13.Rd')
+load('bonf-CIs-nondiff-2017-08-14.Rd')
 results_nondiff <- results
 rm(results)
 
-get_coverage <- function(x, CIs){
-  lower <- apply(CIs, 1, min)
-  lower[is.na(lower)] <- Inf
-  upper <- apply(CIs, 1, max)
-  upper[is.na(upper)] <- -Inf
-  sapply(x, function(x)  mean((lower < x) & (upper > x)))
-}
+op <- par()
 
-CI_plot <- function(results, i, param = 'b', mult = 1, nx = 500, nominal = 0.9){
-  stopifnot(param %in% c('b', 'theta1', 's'))
-
-  true_params <- results$params[i,]
-  b_true <- true_params$b
-  a0_true <- true_params$a0
-  a1_true <- true_params$a1
-  s_true <- 1 - a0_true - a1_true
-  n_true <- true_params$n
-  theta1_true <- b_true / s_true
-  d_true <- true_params$d
-  RF_true <- b_true * (1 - 2 * d_true)
-
-  CIs <- results$CIs_bonf[[i]]
-  CIs <- lapply(CIs, function(x) getElement(x, param))
-  CIs <- do.call(rbind, CIs)
-
-  if(param == 's'){
-    x <- seq(0, 0.999, length.out = nx / 4)
-  } else {
-    xmin <- min(RF_true, theta1_true)
-    xmax <- max(RF_true, theta1_true)
-    xwidth <- max(xmax - xmin, 1)
-    x <- seq(xmin - mult * xwidth, xmax + mult * xwidth, length.out = nx)
-  }
-  coverage <- get_coverage(x, CIs)
-
-  if(param == 'b') {
-    myxlab <- '$\\beta$'
-    param_true <- b_true
-  } else if(param == 'theta1') {
-    myxlab <- '$\\theta_1$'
-    param_true <- theta1_true
-  } else {
-    myxlab <- '$s$'
-    param_true <- s_true
-  }
-  mymain <- paste0('$\\beta = ', b_true,
-                   ', \\alpha_0 = ', a0_true,
-                   ', \\alpha_1 = ', a1_true,
-                   ', n = ', n_true, '$')
-  plot(x, coverage, type = 'l', xlab = myxlab, ylab = 'Coverage',
-       main = mymain)
-  abline(v = param_true, lty = 2)
-  abline(h = nominal, lty = 2)
-
-  if(param == 'b' & (RF_true != theta1_true)){
-    abline(v = RF_true, col = 'red')
-    abline(v = theta1_true, col = 'blue')
-  } else if(param == 'theta1') {
-    abline(v = b_true, col = 'blue')
-  } else {
-    #abline(v = 1 - (2 * d_true), col = 'red')
-    #abline(v = 1, col = 'blue')
+panel_plot <- function(myb, myn, xlims = NULL){
+  par(mfrow = c(3, 3), oma = c(0, 0, 0, 0), pty = 's',
+      mar = c(2.1, 2.1, 2.1, 0.1))
+  indices <- as.numeric(rownames(subset(results_weak$params, n == myn & b == myb &
+                                        a0 != 0.3 & a1 != 0.3)))
+  for(i in indices){
+    CI_compare_plot(results_weak, results_nondiff, i,
+                    xlims = xlims, alphaonly = TRUE)
   }
 }
 
-CI_plot(results_nondiff, 27, param = 'b')
-CI_plot(results_weak, 68, param = 'b')
-CI_plot(results_weak_d1small, 68, param = 'b')
-CI_plot(results_weak, 80, param = 'b')
-CI_plot(results_weak_d1small, 80, param = 'b')
-CI_plot(results_weak, 115, param = 'b')
-CI_plot(results_weak_d1small, 115, param = 'b')
-CI_plot(results_weak, 35, param = 'b')
-CI_plot(results_weak_d1small, 35, param = 'b')
-CI_plot(results_weak, 31, param = 'b', mult = 1)
-CI_plot(results_weak_d1small, 31, param = 'b', mult = 1)
+
+setwd('~/binary-regressor/fig')
+tikzwidth <- 6.5
+tikzheight <- 6.5
+
+tikz(file = 'bonf_zero_1000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(0, 1000, xlim = c(-0.6, 0.6))
+dev.off()
+
+tikz(file = 'bonf_zero_2000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(0, 2000, xlim = c(-0.5, 0.5))
+dev.off()
+
+tikz(file = 'bonf_quarter_1000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(0.25, 1000, xlim = c(-0.2, 1))
+dev.off()
+
+tikz(file = 'bonf_quarter_2000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(0.25, 2000, xlim = c(-0.1, 0.9))
+dev.off()
+
+tikz(file = 'bonf_half_1000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(0.5, 1000, xlim = c(0, 1.5))
+dev.off()
+
+tikz(file = 'bonf_half_2000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(0.5, 2000, xlim = c(0.1, 1.3))
+dev.off()
+
+tikz(file = 'bonf_one_1000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(1, 1000, xlim = c(0.3, 2.5))
+dev.off()
+
+tikz(file = 'bonf_one_2000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(1, 2000, xlim = c(0.4, 2.2))
+dev.off()
+
+tikz(file = 'bonf_two_1000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(2, 1000, xlim = c(0.8, 4))
+dev.off()
+
+tikz(file = 'bonf_two_2000.tex', width = tikzwidth, height = tikzheight)
+panel_plot(2, 2000, xlim = c(1, 3.5))
+dev.off()
+
+par(op)
+rm(list = ls())
 
