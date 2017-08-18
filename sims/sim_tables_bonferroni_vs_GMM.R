a8b57a98e70135f11fe88d9a74381b268cfd41df
Francis DiTraglia <fditra@sas.upenn.edu>
2017-08-18 16:39:04 -0400

    results for twostep CIs

---
+++ sims/sim_tables_bonferroni_vs_GMM.R
@@ -0,0 +1,68 @@
+library(mbereg)
+setwd('~/binary-regressor/sims/')
+load('bonf-vs-gmm-CIs-nondiff-2017-08-17.Rd')
+
+setwd('~/binary-regressor/tab')
+
+#------------------------------------
+# GMM Confidence Intervals
+#------------------------------------
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 1000),
+                   out_stat = 'cover_GMM')),
+    file = 'GMM_CIs_cover_1000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 1000),
+                   out_stat = 'width_GMM')),
+    file = 'GMM_CIs_width_1000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 2000),
+                   out_stat = 'cover_GMM')),
+    file = 'GMM_CIs_cover_2000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 2000),
+                   out_stat = 'width_GMM')),
+    file = 'GMM_CIs_width_2000.tex')
+
+#------------------------------------
+# Bonferroni Confidence Intervals
+#------------------------------------
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 1000),
+                   out_stat = 'cover_bonf')),
+    file = 'bonf_CIs_cover_1000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 1000),
+                   out_stat = 'width_bonf')),
+    file = 'bonf_CIs_width_1000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 2000),
+                   out_stat = 'cover_bonf')),
+    file = 'bonf_CIs_cover_2000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 2000),
+                   out_stat = 'width_bonf')),
+    file = 'bonf_CIs_width_2000.tex')
+
+
+#------------------------------------
+# Two-step Confidence intervals
+#------------------------------------
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 1000),
+                   out_stat = 'cover_twostep')),
+    file = 'twostep_CIs_cover_1000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 1000),
+                   out_stat = 'width_twostep')),
+    file = 'twostep_CIs_width_1000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 2000),
+                   out_stat = 'cover_twostep')),
+    file = 'twostep_CIs_cover_2000.tex')
+
+cat(TeXtable(build_table(subset(summarize_CI_results(results), n == 2000),
+                   out_stat = 'width_twostep')),
+    file = 'twostep_CIs_width_2000.tex')
+
+
+
+rm(list = ls())
